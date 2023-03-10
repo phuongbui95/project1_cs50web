a44cdc3fe8a1145fe2a44ffd2af232767dece3e5
PB Github <phuong.buitan95@gmail.com>
2023-03-10 20:18:59 +0700

    all changed

---
+++ encyclopedia/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+from . import util
+
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def wiki(request, title):
+    return render(request, "encyclopedia/wiki.html", {
+        "get_entry": util.get_entry(title),
+        "title": title,
+        "entries": util.list_entries()
+    })
+
