05edef59383852e80274eaf0cc304b22c3af5795
PB Github <phuong.buitan95@gmail.com>
2023-03-15 18:09:21 +0700

    Update search.html

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("entry/<str:title>", views.entry, name="entry"),
+    path("wiki/<str:title>", views.entry, name="entry"),
+    path("search/", views.search, name="search"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,17 +1,34 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
 from . import util
 
+entries = util.list_entries()
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": entries
     })
+
 
 def entry(request, title):
     return render(request, "encyclopedia/entry.html", {
         "get_entry": util.get_entry(title),
         "title": title,
-        "entries": util.list_entries()
+        "entries": entries
     })
+
+def search(request):
+    # Check if method is GET
+    if request.method == "GET":
+        query = request.GET.get("q") # get the search query
+        if(query in entries):
+            return redirect("encyclopedia:entry", title=query) # redirect to a url tag with 'title' as a parameter
+        return render(request, "encyclopedia/search.html", {
+            "entries": entries,
+            "q": query
+        })
+    
+    return render(request, "encyclopedia/search.html", {
+        "entries": entries,
+        "q": None
+    })            
 
