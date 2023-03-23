a982d6dba1e76ff6177c021c013a54663ef5aaf4
PB Github <phuong.buitan95@gmail.com>
2023-03-23 19:49:19 +0700

    Update static_root

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/entry/<str:title>", views.entry, name="entry"),
+    path("wiki/<str:title>", views.entry, name="entry"),
     path("wiki/search/", views.search, name="search"),
     path("wiki/new/", views.new, name="new"),
     path("wiki/edit/<str:title>", views.edit, name="edit"),
