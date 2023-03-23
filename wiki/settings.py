5c474085fccc0698b99b88c011cc5ca7b2af3684
PB Github <phuong.buitan95@gmail.com>
2023-03-23 20:18:53 +0700

    working on static files

--- wiki/settings.py
+++ wiki/settings.py
@@ -119,4 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# working on this
+STATIC_ROOT = os.path.join(BASE_DIR, 'encyclopedia/static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), ]
